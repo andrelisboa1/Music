1b803048952be64049960c44d2efb2a90da5f014
andrelisboa1 <201902856@estudantes.ips.pt>
2025-07-06 19:15:07 +0100

    Small update for debugging purposes.

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function changeMain(filename) {
     readFromGithubFile(filename)
         .then(content => {
             let m = document.getElementById("main-sec");
+            console.log(content);
             m.innerHTML = content;
         })
         .catch(error => {
