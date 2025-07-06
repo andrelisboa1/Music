6b74260858a6dae32bc25adec0e84b4e91470d96
andrelisboa1 <201902856@estudantes.ips.pt>
2025-07-06 18:27:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-function readHTML(filename) {
-    
+function readFromGithubFile(filename) {
+    let prefix = "raw.githubusercontent.com/andrelisboa1/Music/refs/heads/main/";
+
+    let content = "";
+
+    return fetch(`https://${prefix}${filename}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        });
+}
+
+function setHTMLFromString(parentElement, str) {
+    parentElement.innerHTML = str;
+}
+
+function debugRead(filename) {
+    readFromGithubFile(filename)
+        .then(content => {
+            console.log(content);
+        })
+        .catch(error => {
+            console.error('Error reading file:', error);
+        });
+}
+
+window.onload = function() {
+    debugRead("m_main.html");
 }
